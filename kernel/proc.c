22a0c33908fdbb4281e1e96322d8832ffdeb980a
Your Name <you@example.com>
2022-07-29 12:30:24 +0800

    ready to submit my lab

--- kernel/proc.c
+++ kernel/proc.c
@@ -26,7 +26,7 @@ void
 procinit(void)
 {
   struct proc *p;
-  
+
   initlock(&pid_lock, "nextpid");
   for(p = proc; p < &proc[NPROC]; p++) {
       initlock(&p->lock, "proc");
@@ -76,7 +76,7 @@ myproc(void) {
 int
 allocpid() {
   int pid;
-  
+
   acquire(&pid_lock);
   pid = nextpid;
   nextpid = nextpid + 1;
@@ -127,6 +127,17 @@ found:
   p->context.ra = (uint64)forkret;
   p->context.sp = p->kstack + PGSIZE;
 
+  // Allocate a trapframe page.
+  if((p->alarmtrapframe = (struct trapframe *)kalloc()) == 0){
+      release(&p->lock);
+      return 0;
+  }
+
+  p->tickspass = 0;
+  p->alarmticks = 0;
+  p->alarmhandler = 0;
+  p->accessable = 1;
+
   return p;
 }
 
@@ -141,6 +152,9 @@ freeproc(struct proc *p)
   p->trapframe = 0;
   if(p->pagetable)
     proc_freepagetable(p->pagetable, p->sz);
+  if(p->alarmtrapframe)
+      kfree((void*)p->alarmtrapframe);
+  p->alarmtrapframe=0;
   p->pagetable = 0;
   p->sz = 0;
   p->pid = 0;
@@ -215,7 +229,7 @@ userinit(void)
 
   p = allocproc();
   initproc = p;
-  
+
   // allocate one user page and copy init's instructions
   // and data into it.
   uvminit(p->pagetable, initcode, sizeof(initcode));
@@ -369,7 +383,7 @@ exit(int status)
   acquire(&p->lock);
   struct proc *original_parent = p->parent;
   release(&p->lock);
-  
+
   // we need the parent's lock in order to wake it up from wait().
   // the parent-then-child rule says we have to lock it first.
   acquire(&original_parent->lock);
@@ -440,7 +454,7 @@ wait(uint64 addr)
       release(&p->lock);
       return -1;
     }
-    
+
     // Wait for a child to exit.
     sleep(p, &p->lock);  //DOC: wait-sleep
   }
@@ -458,12 +472,12 @@ scheduler(void)
 {
   struct proc *p;
   struct cpu *c = mycpu();
-  
+
   c->proc = 0;
   for(;;){
     // Avoid deadlock by ensuring that devices can interrupt.
     intr_on();
-    
+
     int found = 0;
     for(p = proc; p < &proc[NPROC]; p++) {
       acquire(&p->lock);
@@ -559,7 +573,7 @@ void
 sleep(void *chan, struct spinlock *lk)
 {
   struct proc *p = myproc();
-  
+
   // Must acquire p->lock in order to
   // change p->state and then call sched.
   // Once we hold p->lock, we can be
--- kernel/sysproc.c
+++ kernel/sysproc.c
@@ -57,7 +57,7 @@ sys_sleep(void)
 {
   int n;
   uint ticks0;
-
+  backtrace();
   if(argint(0, &n) < 0)
     return -1;
   acquire(&tickslock);
@@ -94,4 +94,34 @@ sys_uptime(void)
   xticks = ticks;
   release(&tickslock);
   return xticks;
+}
+uint64
+sys_sigalarm(void)
+{
+  int ticks = 0;
+  uint64 alarmhandler = 0;
+
+  if(argint(0, &ticks) < 0 || argaddr(1, &alarmhandler) < 0)
+      return -1;
+
+  struct proc *p = myproc();
+  p->alarmticks = ticks;
+  p->alarmhandler = alarmhandler;
+  p->tickspass = 0;
+  p->accessable = 1;
+
+  return 0;
+}
+
+uint64
+sys_sigreturn(void)
+{
+    struct proc *p = myproc();
+    // 恢复系统调用时用户的栈帧
+    if (0 == p->accessable) {
+        memmove(p->trapframe, p->alarmtrapframe, sizeof(struct  trapframe));
+        p->accessable = 1;
+    }
+
+    return 0;
 }
